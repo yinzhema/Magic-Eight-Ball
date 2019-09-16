d42c40a9ad074b5df73222aded77baa373ad3bbb
yinzhema <yma9826@gmail.com>
2019-09-16 17:43:59 -0400

    Initial Commit

---
+++ Magic Eight Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic Eight Ball
+//
+//  Created by Yinzhe Ma on 9/16/19.
+//  Copyright © 2019 Yinzhe Ma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
