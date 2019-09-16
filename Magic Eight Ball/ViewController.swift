5f1e0b04a8a0e106d6cfef6f68f974d145dcd031
yinzhema <yma9826@gmail.com>
2019-09-16 18:27:47 -0400

    version 1 complete

--- Magic Eight Ball/ViewController.swift
+++ Magic Eight Ball/ViewController.swift
@@ -9,11 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func asnwerButtonpRESSED(_ sender: UIButton) {
+        let messages=["It is certain",
+                      "It is decidedly so",
+                      "Without a doubt",
+                      "Yes, definitely",
+                      "You may rely on it",
+                      "As I see it, yes",
+                      "Most likely",
+                      "Outlook good",
+                      "Yes",
+                      "Signs point to yes",
+                      "Reply hazy try again",
+                      "Ask again later",
+                      "Better not tell you now",
+                      "Cannot predict now",
+                      "Concentrate and ask again",
+                      "Don't count on it",
+                      "My reply is no",
+                      "My sources say no",
+                      "Outlook not so good",
+                      "Very doubtful"
+        ]
+        resultLabel.text=messages.randomElement()
+    }
+    
 
 
 }
